added613618e4b57d780ae73bcb49fdf62f78488
Andrzej Kapolka <drzej.k@gmail.com>
2022-08-30 17:03:29 -0700

    Starting on 2018 AOC in Haskell.

---
+++ 2018/1/main.hs
@@ -0,0 +1,14 @@
+import qualified Data.Set as Set
+
+readNumber :: [Char] -> Int
+readNumber s = read (if head s == '+' then tail s else s)
+
+firstRepeated :: [Int] -> Set.Set Int -> Int
+firstRepeated list seen
+  | Set.member value seen = value
+  | otherwise = firstRepeated (tail list) (Set.insert value seen)
+  where value = head list
+
+main = do
+  contents <- readFile "input.txt"
+  putStrLn (show (firstRepeated (scanl (+) 0 (cycle (map readNumber (lines contents)))) Set.empty))
